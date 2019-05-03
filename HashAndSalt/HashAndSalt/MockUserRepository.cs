0a6453088fa255928c476bf6c2e4a360bc8846ed
Franki Micheo <46354809+frankiaydan867@users.noreply.github.com>
2019-05-03 08:28:06 -0400

    Add files via upload

---
+++ HashAndSalt/HashAndSalt/MockUserRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace HashAndSalt
+{
+    class MockUserRepository
+    {
+        List<User> users = new List<User>();
+
+        // Function to add the user to im memory dummy DB
+        public void AddUser(User user)
+        {
+            users.Add(user);
+        }
+
+        // Function to retrieve the user based on user id
+        public User GetUser(string userid)
+        {
+            return users.Single(u => u.UserId == userid);
+        }
+
+    }
+}
---
+++ HashAndSalt/HashAndSalt/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashAndSalt
+{
+    class User
+    {
+        public string UserId { get; set; }
+        public string PasswordHash { get; set; }
+        public string Salt { get; set; }
+
+    }
+}
